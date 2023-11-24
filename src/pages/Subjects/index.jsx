53a203b3b3f6ffe19a4ee07a9cd569f380b39726
Kirill Kuzmich <kirill.kuzmich.03@mail.ru>
2023-11-24 13:50:47 +0300

    added other files

---
+++ src/pages/Subjects/index.jsx
@@ -0,0 +1,50 @@
+import React from 'react';
+import cl from "./Subjects.module.scss";
+import {useDispatch, useSelector} from "react-redux";
+import {Subject, TagsBlock} from "../../components";
+import {fetchSubjects, fetchTags} from "../../redux/slices/subjects";
+
+
+export const Subjects = () => {
+
+    const dispatch = useDispatch();
+    const useData = useSelector(state => state.auth.data)
+    const {subjects, tags} = useSelector(state => state.subjects)
+
+    const isSubjectsLoading = subjects.status === 'loading';
+    const isTagsLoading = tags.status === 'loading';
+
+    React.useEffect(() => {
+        dispatch(fetchSubjects())
+        dispatch(fetchTags())
+    }, [])
+
+
+    return (
+        <>
+            <div className={cl.gridContainer}>
+                <div className={cl.postContainer}>
+                    {(subjects.items).map((obj, index) =>
+                        isSubjectsLoading ? (
+                            <Subject key={index} isLoading={true}/>
+                        ) : (
+                            <Subject
+                                id={obj._id}
+                                title={obj.title}
+                                user={obj.user}
+                                createdAt={obj.createdAt}
+                                tags={obj.tags}
+                                isEditable={useData?._id === obj.user._id}
+                            />
+                        )
+                    )}
+                </div>
+                <div className={cl.sidebar}>
+                    <TagsBlock items={tags.items} isLoading={isTagsLoading}/>
+                </div>
+            </div>
+        </>
+    );
+
+
+};
---
+++ src/redux/slices/subjects.js
@@ -0,0 +1,68 @@
+import {createSlice, createAsyncThunk} from "@reduxjs/toolkit";
+import axios from "../../axios";
+
+
+export const fetchSubjects = createAsyncThunk('subjects/fetchSubjects', async () => {
+    const {data} = await axios.get('/subjects')
+    return data
+})
+export const fetchTags = createAsyncThunk('subjects/fetchTags', async () => {
+    const {data} = await axios.get('/subject-tags')
+    return data
+})
+export const fetchRemoveSubject = createAsyncThunk('subjects/fetchRemoveSubject', (id) => {
+    axios.delete(`/subjects/${id}`)
+})
+
+const initialState = {
+    subjects: {
+        items: [],
+        status: "loading"
+    },
+    tags: {
+        items: [],
+        status: "loading"
+    },
+
+};
+
+const subjectsSlice = createSlice({
+    name: 'subjects',
+    initialState,
+    reducers: {},
+    extraReducers: {
+        [fetchSubjects.pending]: handlePending('subjects'),
+        [fetchSubjects.fulfilled]: handleFulfilled('subjects'),
+        [fetchSubjects.rejected]: handleRejected('subjects'),
+        [fetchTags.pending]: handlePending('tags'),
+        [fetchTags.fulfilled]: handleFulfilled('tags'),
+        [fetchTags.rejected]: handleRejected('tags'),
+        [fetchRemoveSubject.pending]: handlePendingForRemove('subjects'),
+    }
+})
+
+function handlePendingForRemove(sliceName) {
+    return (state, action) => {
+        state[sliceName].items = state.posts.items.filter(obj=>obj._id !== action.meta.arg);
+    };
+}
+function handlePending(sliceName) {
+    return (state) => {
+        state[sliceName].items = [];
+        state[sliceName].status = 'loading';
+    };
+}
+function handleFulfilled(sliceName) {
+    return (state, action) => {
+        state[sliceName].items = action.payload;
+        state[sliceName].status = 'loaded';
+    };
+}
+function handleRejected(sliceName) {
+    return (state) => {
+        state[sliceName].items = [];
+        state[sliceName].status = 'error';
+    };
+}
+
+export const subjectsReducer = subjectsSlice.reducer
